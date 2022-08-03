0cebd66bce91a49584c54255a0f90cd93b15a989
Mahmoud Alkawteb <mahmoud.alkateb22@gmail.com>
2022-08-03 14:39:50 +0200

    added the dataBase class for room RunningDataBase.kt

--- app/src/main/java/com/androiddevs/runningappyt/db/RunningDataBase.kt
+++ app/src/main/java/com/androiddevs/runningappyt/db/RunningDataBase.kt
@@ -2,12 +2,14 @@ package com.androiddevs.runningappyt.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 
 @Database(
     entities = [Run::class],
     version = 1
 )
+@TypeConverters(Convertors::class)
 abstract class RunningDataBase : RoomDatabase(){
 
    abstract fun getRunDao() : RunDao

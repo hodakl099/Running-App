8f3fef07d4db484a89de48f2d8bfe6415cd77991
Mahmoud Alkawteb <mahmoud.alkateb22@gmail.com>
2022-08-03 14:32:59 +0200

    added  Convertors.kt to convert bitmap to a byte array

--- app/src/main/java/com/androiddevs/runningappyt/db/Convertors.kt
+++ app/src/main/java/com/androiddevs/runningappyt/db/Convertors.kt
@@ -2,14 +2,17 @@ package com.androiddevs.runningappyt.db
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import androidx.room.TypeConverter
 import java.io.ByteArrayOutputStream
 
 class Convertors {
 
+    @TypeConverter
     fun toBitMap(byte: ByteArray) : Bitmap{
         return BitmapFactory.decodeByteArray(byte,0,byte.size)
     }
 
+    @TypeConverter
     fun fromBytes(bmp: Bitmap) : ByteArray {
         val outputStream = ByteArrayOutputStream()
         bmp.compress(Bitmap.CompressFormat.PNG, 100, outputStream)

da1d26245872ac4d894fb0319907e3ed15900957
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-08-04 04:53:07 +0200

    added the BaseApplication.kt to Manifest

---
+++ app/src/main/java/com/androiddevs/runningappyt/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.androiddevs.runningappyt.di
+
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import com.androiddevs.runningappyt.db.RunningDataBase
+import com.androiddevs.runningappyt.other.Constants.RUNNING_DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRunningDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        RunningDataBase::class.java,
+        RUNNING_DATABASE_NAME,
+    ).build()
+
+    @Singleton
+    @Provides
+    fun providesRunDao(db : RunningDataBase) = db.getRunDao()
+
+}

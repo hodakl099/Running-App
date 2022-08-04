8dc45bc1a7cf5ffce5e526332a71a9fec829ef0e
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-08-04 05:45:03 +0200

    added the StatisticsFragment.kt

---
+++ app/src/main/java/com/androiddevs/runningappyt/ui/fragments/StatisticsFragment.kt
@@ -0,0 +1,7 @@
+package com.androiddevs.runningappyt.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.runningappyt.R
+
+class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
+}

d6b5d819e52c47985712369c8af3cf3d8a0cb1c5
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-08-04 17:44:55 +0200

    added

--- app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
@@ -10,7 +10,10 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {

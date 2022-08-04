c1facb484729f60957ab2ef9ecb31eb2a28769fa
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-08-04 19:51:21 +0200

    added the MapView in the TrackingFragment.kt

--- app/src/main/java/com/androiddevs/runningappyt/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/fragments/TrackingFragment.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import com.androiddevs.runningappyt.R
 import com.androiddevs.runningappyt.ui.viewodels.MainViewModel
 import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_run.*
 import kotlinx.android.synthetic.main.fragment_tracking.*
 
 @AndroidEntryPoint
@@ -17,9 +15,11 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel : MainViewModel by viewModels()
 
-    private var map : GoogleMap? = null
+    private var map: GoogleMap? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
 
         mapView.getMapAsync {
             map = it

7064e24b339871bbcb983da8e5d6299ddce5590c
Mahmoud Alkawteb <mahmoud.alkateb22@gmail.com>
2022-08-06 19:06:44 +0200

    added the foreground service to TrackingService.kt

--- app/src/main/java/com/androiddevs/runningappyt/other/Constants.kt
+++ app/src/main/java/com/androiddevs/runningappyt/other/Constants.kt
@@ -13,4 +13,8 @@ object Constants {
     const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
 
     const val ACTION_SHOW_TRACKING_FRAGMENT = "ACTION_SHOW_TRACKING_FRAGMENT"
+
+    const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
+    const val NOTIFICATION_CHANNEL_NAME = "Tracking"
+    const val NOTIFICATION_ID = 1
 }

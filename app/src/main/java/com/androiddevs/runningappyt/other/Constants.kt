4cf20199f1ee241bfc4e44683294af3cbad8ffaf
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-08-07 07:05:24 +0200

    String file is Updated

--- app/src/main/java/com/androiddevs/runningappyt/other/Constants.kt
+++ app/src/main/java/com/androiddevs/runningappyt/other/Constants.kt
@@ -16,9 +16,12 @@ object Constants {
 
 
     const val LOCATION_UPDATE_INTERVAL = 5000L
+
     const val FASTEST_LOCATION_INTERVAL = 2000L
 
     const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
+
     const val NOTIFICATION_CHANNEL_NAME = "Tracking"
+
     const val NOTIFICATION_ID = 1
 }

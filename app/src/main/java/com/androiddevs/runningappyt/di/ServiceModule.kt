89f874eb46e106a37b8ea8d82e37582ad8a69ea3
Mahmoud Alkawteb <mahmoud.alkateb22@gmail.com>
2022-08-08 17:44:57 +0200

    new dagger module ServiceModule.kt for TrackingService.kt

---
+++ app/src/main/java/com/androiddevs/runningappyt/di/ServiceModule.kt
@@ -0,0 +1,11 @@
+package com.androiddevs.runningappyt.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+}

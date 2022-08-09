b477e5b644681344bf591239d1cb047390cd417a
Mahmoud Alkawteb <mahmoud.alkateb22@gmail.com>
2022-08-09 18:04:12 +0200

    added the new function provideMainActivityPendingIntent ServiceModule.kt

--- app/src/main/java/com/androiddevs/runningappyt/di/ServiceModule.kt
+++ app/src/main/java/com/androiddevs/runningappyt/di/ServiceModule.kt
@@ -1,11 +1,44 @@
 package com.androiddevs.runningappyt.di
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import com.androiddevs.runningappyt.other.Constants
+import com.androiddevs.runningappyt.ui.MainActivity
+import com.google.android.gms.location.FusedLocationProviderClient
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
 
 
 @Module
 @InstallIn(ServiceComponent::class)
 object ServiceModule {
+
+
+    @SuppressLint("VisibleForTests")
+    @ServiceScoped
+    @Provides
+   fun provideFusedLocationProviderClient(
+    @ApplicationContext context: Context
+    ) = FusedLocationProviderClient(context)
+
+
+    @ServiceScoped
+    @Provides
+    fun provideMainActivityPendingIntent(
+        @ApplicationContext context: Context
+    ) = PendingIntent.getActivity(
+        context,
+        0,
+        Intent(context, MainActivity::class.java).also {
+            it.action = Constants.ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
 }

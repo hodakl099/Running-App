2063da5242107c148eb96f6bd0fc8d5e6e6a2d52
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-08-09 10:27:35 +0200

    new functions to TrackingService.kt.kt

--- app/src/main/java/com/androiddevs/runningappyt/other/Constants.kt
+++ app/src/main/java/com/androiddevs/runningappyt/other/Constants.kt
@@ -18,13 +18,11 @@ object Constants {
 
     const val TIMER_UPDATE_INTERVAL = 50L
 
-
     const val POLYLINE_COLOR = Color.BLUE
 
     const val POLYLINE_WIDTH = 8f
 
     const val MAP_ZOOM = 15f
-
 
     const val LOCATION_UPDATE_INTERVAL = 5000L
 

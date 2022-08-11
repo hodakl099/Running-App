4035b16cc8acede91e4b097dbcf194018c473aad
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-08-11 18:39:51 +0200

    TrackingService.kt

--- app/src/main/java/com/androiddevs/runningappyt/di/ServiceModule.kt
+++ app/src/main/java/com/androiddevs/runningappyt/di/ServiceModule.kt
@@ -16,28 +16,24 @@ import dagger.hilt.android.components.ServiceComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ServiceScoped
 
-
 @Module
 @InstallIn(ServiceComponent::class)
 object ServiceModule {
 
-
-    @SuppressLint("VisibleForTests")
     @ServiceScoped
     @Provides
-   fun provideFusedLocationProviderClient(
-    @ApplicationContext context: Context
-    ) = FusedLocationProviderClient(context)
-
+    fun provideFusedLocationProviderClient(
+        @ApplicationContext app: Context
+    ) = FusedLocationProviderClient(app)
 
     @ServiceScoped
     @Provides
     fun provideMainActivityPendingIntent(
-        @ApplicationContext context: Context
+        @ApplicationContext app: Context
     ) = PendingIntent.getActivity(
-        context,
+        app,
         0,
-        Intent(context, MainActivity::class.java).also {
+        Intent(app, MainActivity::class.java).also {
             it.action = Constants.ACTION_SHOW_TRACKING_FRAGMENT
         },
         PendingIntent.FLAG_UPDATE_CURRENT
@@ -55,5 +51,4 @@ object ServiceModule {
         .setContentTitle("Running App")
         .setContentText("00:00:00")
         .setContentIntent(pendingIntent)
-
 }
